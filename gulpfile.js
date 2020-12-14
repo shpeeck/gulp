80d0283d2a3f36bf8ab4a761146cc7bc1d5b8816
shpeeck <jenya_vas@mail.ru>
2020-12-14 13:33:10 +0200

    gulp-sass, watcher and all package

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const { src, dest, watch } = require("gulp");
+const scss = require("gulp-sass"),
+	prefix = require("gulp-autoprefixer");
+
+function convertStyles() {
+	return src('app/scss/style.scss')
+	.pipe(scss(
+        {
+            outputStyle: 'compressed'
+        }
+    ))
+    .pipe(prefix())
+	.pipe(dest('dist/css'));
+};
+
+function watchFiles() {
+    watch('app/scss/**/*.scss', convertStyles);
+    // watch('app/*.html').on("change", sync.reload);
+    // watch('app/css/*.css').on("change", sync.reload);
+    // watch('app/js/*.js').on("change", sync.reload);
+    // watch('app/_img', imagesCompressed);
+    // watch('app/fonts/**.ttf', series(convertFonts, fontsStyle));
+}
+
+
+exports.convertStyles = convertStyles;
+exports.watchFiles = watchFiles;
